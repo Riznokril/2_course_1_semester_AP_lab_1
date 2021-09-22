d32e83126bbbddf061f53e9876ba89a04507b8f5
Riznokril <48068349+Riznokril@users.noreply.github.com>
2021-09-22 21:07:50 +0300

    Сhanges at the request of the inspector

--- tests.py
+++ tests.py
@@ -5,13 +5,8 @@ class TestHeapSort(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(2+2, 5)
-    #cmd doesn't see first test(even if it obviously false)
-    #and I don't know why, so I added this
-
-    def test_sort(self):
-        arr = [5, 4, 6, 1, 2, 3]
-        heap_sort(arr, "asc")
-        self.assertEqual(arr, [1, 2, 3, 4, 5, 6])
+    # cmd doesn't see first test(even if it obviously false)
+    # and I don't know why, so I added this
 
     def test_desc_to_asc(self):
         arr = [6, 5, 4, 3, 2, 1]
@@ -32,6 +27,11 @@ class TestHeapSort(unittest.TestCase):
         arr = [6, 5, 4, 3, 2, 1]
         heap_sort(arr, "desc")
         self.assertEqual(arr, [6, 5, 4, 3, 2, 1])
+
+    def test_sort_1(self):
+        arr = [5, 4, 6, 1, 2, 3]
+        heap_sort(arr, "asc")
+        self.assertEqual(arr, [1, 2, 3, 4, 5, 6])
 
 if __name__ == "__main__":
   unittest.main()
